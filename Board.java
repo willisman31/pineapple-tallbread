bb115d249e9b02f0b54f5d636f96352dc09bf9ba
willisman31 <58826505+willisman31@users.noreply.github.com>
2021-04-07 07:59:13 -0400

    Add files via upload

--- Board.java
+++ Board.java
@@ -6,20 +6,65 @@
 public class Board {
 
 	private Piece[][] board;
-	
+
 	/**
 	 * Default constructor.
 	 */
 	Board() {
-		// TODO
-		
+		this.board = new Piece[8][8];
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (i % 7 == 0) {
+					board[i][j] = new Rook();
+					board[i][j] = new Knight();
+					board[i][j] = new Bishop();
+					board[i][j] = new King();
+					board[i][j] = new Queen();
+					board[i][j] = new Bishop();
+					board[i][j] = new Knight();
+					board[i][j] = new Rook();
+				} else if (i == 1 || i == 7) {
+					board[i][j] = new Pawn();
+				} else {
+					board[i][j] = null;
+				}
+			}
+		}
 	}
 	
 	/**
+	 * Copy constructor.
+	 * @param board to be copied.
+	 */
+	Board(Board board) {
+		// TODO
+	}
+	
+	/**
+	 * Create special instance of board.
+	 * @param board to be created as board.
+	 */
+	Board(Piece[][] board) {
+		// TODO
+	}
+
+	/**
 	 * Board getter.
+	 * 
 	 * @return current state of this board.
 	 */
 	public Piece[][] getBoard() {
 		return this.board;
 	}
+	
+	/**
+	 * 
+	 * @param row
+	 * @param col
+	 * @return
+	 */
+	public Piece getSpace(int row, int col) {
+		return this.board[row][col];
+	}
+	
 }
